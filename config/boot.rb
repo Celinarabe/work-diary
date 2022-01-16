8aa0004cd3a58affab7d378b22556fe650be45a8
Celina Rabe <celinarabe@celinas-air.attlocal.net>
2022-01-16 17:14:46 -0600

    maybe?

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
 # begin
-# require "bootsnap/setup" # Speed up boot time by caching expensive operations.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
 # rescue LoadError
 #   # bootsnap is an optional dependency, so if we don't have it it's fine
 #   # Do not load in production because file system (where cache would be written) is read-only
