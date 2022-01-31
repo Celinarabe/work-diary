7bf17915716e662e5b1458cf3ad7f2f88e16df42
Celina Rabe <celinarabe@celinas-air.attlocal.net>
2022-01-30 19:42:30 -0600

    new post api route

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,33 @@
-class EntriesController < ApplicationController
+class EntriesController < ActionController::API
 
-    def index
-        render :json => Entry.all
+  def index
+      render :json => Entry.all
+  end
+
+  def show
+    @entry = Entry.find(params[:id])
+    render :json => @entry #idk this syntax D:
+  end
+
+  def create
+    entry = Entry.new(entry_params)
+    
+    if entry.save
+      render json: entry
+    else
+      render json: errors(entry), status: 422
     end
-  
-    def show
-      @entry = Entry.find(params[:id])
-      render :json => @entry
+  end
+
+  private
+    def entry_params
+      params.permit(:author, :date)
     end
+
+    def errors(record)
+      { errors: record.errors.messages }
+    end
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'tasks#index'
+  root 'entries#index'
   resources :entries do
     resources :tasks
   end
